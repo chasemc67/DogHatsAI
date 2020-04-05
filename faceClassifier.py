fbdb73943b61e15a687d9757ffdfc814e04eab08
Chase McCarty <chasemc67@gmail.com>
2020-04-04 18:40:15 -0700

    Initial commit

---
+++ faceClassifier.py
@@ -0,0 +1,17 @@
+import cv2
+from keras.applications.resnet50 import ResNet50
+ResNet50_model_ = ResNet50(weights='imagenet')
+
+from keras.preprocessing import image
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    return np.expand_dims(x, axis=0)
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
