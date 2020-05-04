2fb7a4e27233bbd5813c02797157989200f7e3a7
chasemc67 <chasemc67@gmail.com>
2020-05-03 23:07:27 -0700

    Added line to grab unannotated dog pics too

--- imageMonkeyDownload.py
+++ imageMonkeyDownload.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
 	logging.basicConfig()
 
 	api = API(api_version=1)
-	res = api.export_to_folder(["dog.has='eye'", "dog.has='nose'"], "./Data/AnnotatedData", min_probability = 0.8, only_annotated = True)
+	#res = api.export_to_folder(["dog.has='eye'", "dog.has='nose'"], "./Data/AnnotatedData", min_probability = 0.8, only_annotated = True)
+	api.export_to_folder(["dog"], "./Data/UnannotatedData", min_probability=0.8)
